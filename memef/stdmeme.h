7357b81eeab3f30d7673668e0b11eba042eae33c
mgq79 <matthew.quarles@gmail.com>
2017-11-07 14:37:55 -0500

    The beginning of an epic quest

---
+++ memef/stdmeme.h
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void memef(char *pat, ...);
+
+char *spongeCase(char *str); //SpOnGeCaSe
+char *owOofCase(char *str);  //_o_*w*_O_*o*_f_*C*_a_*s*_e_
+char *aestheticCase(char *str);  // a e s t h e t i c C a s e
+
+

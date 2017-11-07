5d988f0af6f18522e877ddcd4843824ac7b18437
mgq79 <matthew.quarles@gmail.com>
2017-11-07 14:38:46 -0500

    Renaming the epic journey

---
+++ stdmeme/memef.c
@@ -0,0 +1,13 @@
+#include <stdmeme.h>
+#include <stdarg.h>
+
+
+void memef(char *pat, ...)
+{
+
+
+
+
+
+
+}
---
+++ stdmeme/test.c
@@ -0,0 +1,6 @@
+#include "stdmeme.h"
+
+int main(){
+    printf("%s\n", spongeCase("This is a test"));
+    return 0;
+}

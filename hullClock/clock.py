a20aaa1e4e62ff109457272819816162bf8f3976
mgq79 <matthew.quarles@gmail.com>
2018-01-31 14:52:20 -0500

    First working clock with lerped movement

--- hullClock/clock.py
+++ hullClock/clock.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
 from tkinter import *
-import math
+import math, time
 
 def init(data):
-    data.hourRadius   = 75
-    data.minuteRadius = 90
-    data.secondRadius = 85
-    data.hourTickLength = 30
+    data.timerDelay       = 1
+    data.hourRadius       = 180
+    data.minuteRadius     = 215
+    data.secondRadius     = 200
+    data.hourTickLength   = 30
     data.minuteTickLength = 8
-    data.maxRadius = 230
-    data.hourTickColor = 'lightgrey'
-    data.minuteTickColor = 'lightgrey'
-    data.hourTickWidth = 3
-    data.minuteTickWidth = 2
-    data.triColor = 'cyan'
+    data.maxRadius        = 230
+    data.hourTickColor    = 'lightgrey'
+    data.minuteTickColor  = 'lightgrey'
+    data.hourTickWidth    = 3
+    data.minuteTickWidth  = 2
+    data.triColor         = 'cyan'
+    data.hourPoint        = None
+    data.minutePoint      = None
+    data.secondPoint      = None
+    data.lerpRate         = 0.5
 
 def pointOnCircle(data, radius, angle):
     centerX = data.width/2
@@ -22,6 +27,12 @@ def pointOnCircle(data, radius, angle):
     y = radius*math.sin(angle) + centerY
     x = radius*math.cos(angle) + centerX
     return (x,y)
+
+
+def lerp(point0, point1, rate):
+    (x0, y0) = point0
+    (x1, y1) = point1
+    return (x0*rate + x1*(1-rate),y0*rate + y1*(1-rate))
 
 def mousePressed(event, data):
     pass
@@ -55,10 +66,38 @@ def drawClockFace(canvas, data):
         (x1, y1) = pointOnCircle(data, radius1, angle)
         canvas.create_line(x0, y0, x1, y1, fill=data.hourTickColor, width=data.hourTickWidth)
 
+def drawClockTriangle(canvas, data):
+    hour = int(time.strftime("%I"))
+    minute = int(time.strftime("%M"))
+    second = int(time.strftime("%S"))
+
+    hourAngle = (hour-3)*math.pi/6
+    minuteAngle = (minute-15)*math.pi/30
+    secondAngle = (second-15)*math.pi/30
+
+    if(data.hourPoint == None):
+        data.hourPoint   = pointOnCircle(data, data.hourRadius, hourAngle)
+        data.minutePoint = pointOnCircle(data, data.minuteRadius, minuteAngle)
+        data.secondPoint = pointOnCircle(data, data.secondRadius, secondAngle)
+
+
+    data.hourPoint = lerp(data.hourPoint, pointOnCircle(data, data.hourRadius, hourAngle), data.lerpRate)
+    data.minutePoint = lerp(data.minutePoint, pointOnCircle(data, data.minuteRadius, minuteAngle), data.lerpRate)
+    data.secondPoint = lerp(data.secondPoint, pointOnCircle(data, data.secondRadius, secondAngle), data.lerpRate)
+
+    (xh, yh) = data.hourPoint
+    (xm, ym) = data.minutePoint
+    (xs, ys) = data.secondPoint
+
+    canvas.create_line(xh, yh, xm, ym, fill=data.triColor)
+    canvas.create_line(xh, yh, xs, ys, fill=data.triColor)
+    canvas.create_line(xm, ym, xs, ys, fill=data.triColor)
+
 
 
 def redrawAll(canvas, data):
     drawClockFace(canvas,data)
+    drawClockTriangle(canvas, data)
 
 def run(width=300, height=300):
     def redrawAllWrapper(canvas, data):
@@ -102,6 +141,5 @@ def run(width=300, height=300):
     timerFiredWrapper(canvas, data)
     # and launch the app
     root.mainloop()  # blocks until window is closed
-    print("bye!")
 
 run(600, 500)
